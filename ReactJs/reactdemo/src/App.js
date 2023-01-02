fcb49a14e0320e8c24b69f4afa7e32af03fb1a7e
TIWARISUSHMITA <sushmitatiwari.rewa@gmail.com>
2023-01-02 18:54:41 +0530

    okkee all

---
+++ ReactJs/reactdemo/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Demo from './component/Demo';
+
+function App() {
+  return (
+    <div className="App">
+    <Demo/>
+     
+    </div>
+     
+     
+  );
+}
+
+export default App;
---
+++ ReactJs/reactdemo/src/component/Demo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Demo() {
+  return (
+    <div>
+      <h1> Demo React Page</h1>
+    </div>
+  )
+}
+
+export default Demo
